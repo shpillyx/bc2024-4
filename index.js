7974cad93466f823dc6029a3ae5fca2a4a17b861
Юлія bc2024-4 <10juliia10@gmail.com>
2024-11-21 12:40:12 +0200

    Add http server

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { program } = require('commander');
-
+const http = require('http');
 
 program
-    .option('-h, --host <host>', 'server host')
-    .option('-p, --port <port>', 'server port')
-    .option('-c, --cache <cacheDir>', 'cache directory');
+    .option('-h, --host <host>', 'Адреса сервера')
+    .option('-p, --port <port>', 'Порт сервера')
+    .option('-c, --cache <cacheDir>', 'Шлях до директорії з кешем');
 
 program.parse();
 const options = program.opts();
@@ -13,3 +13,12 @@ if (!options.host || !options.port || !options.cache) {
     console.error('Please, specify necessary param');
     process.exit(1);
 }
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Server is working!');
+});
+
+server.listen(options.port, options.host, () => {
+  console.log(`Server running at http://${options.host}:${options.port}/`);
+});
